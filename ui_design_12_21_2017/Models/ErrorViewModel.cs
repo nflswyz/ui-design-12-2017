61864196fa7d0930030b0d75bcd6447614900b21
Yuzhi Wang <nuptwyz@gmail.com>
2017-12-26 11:34:35 -0500

    Status as a enum

---
+++ ui_design_12_21_2017/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ui_design_12_21_2017.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
